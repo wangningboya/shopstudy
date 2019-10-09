a4284370ce69c1846e4fc058333e19f36963b982
wangyong <843096638@qq.com>
2019-10-10 00:07:52 +0800

    消费者注册 动态调用

--- cloud-demo/consumer-demo/src/main/java/cn/wy/consumer/ConsumerApplication.java
+++ cloud-demo/consumer-demo/src/main/java/cn/wy/consumer/ConsumerApplication.java
@@ -3,9 +3,11 @@ package cn.wy.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ConsumerApplication {
 
--- cloud-demo/consumer-demo/src/main/java/cn/wy/consumer/web/ConsumerController.java
+++ cloud-demo/consumer-demo/src/main/java/cn/wy/consumer/web/ConsumerController.java
@@ -3,11 +3,15 @@ package cn.wy.consumer.web;
 
 import cn.wy.consumer.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/consumer")
@@ -16,12 +20,19 @@ public class ConsumerController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @GetMapping(value = "/queryById")
     public User queryById(
-        @RequestParam String id
+            @RequestParam String id
     ) {
-        String url = "http://localhost:8081/user/queryById?id="+id;
-        User user = restTemplate.getForObject(url,User.class);
+        // 根据服务id获取实例
+        List<ServiceInstance> instances = discoveryClient.getInstances("user-service");
+        // 从实例中取出ip和端口
+        ServiceInstance serviceInstance = instances.get(0);
+        String url = "http://" + serviceInstance.getHost() + ":" + serviceInstance.getPort() + "/user/queryById?id=" + id;
+        User user = restTemplate.getForObject(url, User.class);
         return user;
     }
 }
