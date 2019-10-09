6b766a9c67b777f544dee3732d5ba1383464ee68
wangyong <843096638@qq.com>
2019-10-09 23:47:31 +0800

    user eureka注册

--- cloud-demo/user-service/src/main/java/cn/wy/user/UserServiceApplication.java
+++ cloud-demo/user-service/src/main/java/cn/wy/user/UserServiceApplication.java
@@ -2,12 +2,17 @@ package cn.wy.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import tk.mybatis.spring.annotation.MapperScan;
 
 /**
  * @Author WY
  * @Date 2019/9/27
  **/
+//@EnableEurekaClient
+//通用 兼容zookeeper
+@EnableDiscoveryClient
 @SpringBootApplication
 @MapperScan(basePackages = "cn.wy.user.mapper")
 //@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
