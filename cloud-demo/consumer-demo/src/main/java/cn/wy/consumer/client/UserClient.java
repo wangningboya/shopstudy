b60a6d96637d7b3e85ac4a8432833f200f0b91e8
wangningboya <843096638@qq.com>
2019-10-12 15:41:14 +0800

    解决网管启动错误

--- cloud-demo/consumer-demo/src/main/java/cn/wy/consumer/client/UserClient.java
+++ cloud-demo/consumer-demo/src/main/java/cn/wy/consumer/client/UserClient.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 public interface UserClient {
 
     @GetMapping("user/queryById")
-    User queryById(@RequestParam String id);
+    User queryById(@RequestParam("id") String id);
 
 }
